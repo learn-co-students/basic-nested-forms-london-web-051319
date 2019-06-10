c0696d8645becd5dc967b85f2c08242cd8bfcb25
mbarrins <maija.barrins@gmail.com>
2019-06-10 12:32:22 +0100

    Done.

--- db/migrate/20160114112845_add_person_id_to_addresses.rb
+++ db/migrate/20160114112845_add_person_id_to_addresses.rb
@@ -1,4 +1,4 @@
-class AddPersonIdToAddresses < ActiveRecord::Migration
+class AddPersonIdToAddresses < ActiveRecord::Migration[5.2]
   def change
     add_column :addresses, :person_id, :integer
   end
